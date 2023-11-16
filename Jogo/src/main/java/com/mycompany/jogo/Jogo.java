1ea8d3c7a0ac8f607fd0118735453bca0e8cbd8a
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-11-16 19:37:51 -0300

    primeiro try

--- Jogo/src/main/java/com/mycompany/jogo/Jogo.java
+++ Jogo/src/main/java/com/mycompany/jogo/Jogo.java
@@ -11,6 +11,6 @@ package com.mycompany.jogo;
 public class Jogo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Got mitt uns");
     }
 }
